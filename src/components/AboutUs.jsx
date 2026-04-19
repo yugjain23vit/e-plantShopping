203b024a38b939ee8f166459d5b63a2a62ddb896
yugjain23vit <yug.jain23@vit.edu>
2026-04-19 16:10:34 +0530

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+export default function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About e-plantShopping</h2>
+      <p>
+        e-plantShopping is an online platform offering a variety of indoor and outdoor plants.
+      </p>
+      <p>
+        Our mission is to provide high-quality plants and make gardening easy and accessible.
+      </p>
+    </div>
+  );
+}
